7df70bf5537fc99185fc7f99b402492331d01691
José Manuel Barroso Galindo <theypsilon@gmail.com>
2021-01-23 14:37:04 +0100

    Adding rerere support to sync_release.

--- fork_ci_template/.github/sync_release.sh
+++ fork_ci_template/.github/sync_release.sh
@@ -20,6 +20,7 @@ COMMIT_TO_MERGE=$(git log -n 1 --pretty=format:%H -- "releases/${NEW_RELEASE_FIL
 export GIT_MERGE_AUTOEDIT=no
 git config --global user.email "theypsilon@gmail.com"
 git config --global user.name "The CI/CD Bot"
+git config --global rerere.enabled true
 
 echo
 echo "Syncing with upstream:"
@@ -27,6 +28,51 @@ git fetch origin --unshallow 2> /dev/null || true
 git checkout -qf ${MAIN_BRANCH}
 
 ORIGIN_CORE_FILE=$(cd releases/ ; git ls-files -z | xargs -0 -n1 -I{} -- git log -1 --format="%ai {}" {} | grep "${CORE_NAME}" | sort | tail -n1 | awk '{ print substr($0, index($0,$4)) }')
+
+echo
+echo "START rerere-train.sh"
+
+# Remember original branch
+ORIGINAL_BRANCH=$(git symbolic-ref -q HEAD) ||
+ORIGINAL_HEAD=$(git rev-parse --verify HEAD) || {
+	echo >&2 "rerere-train.sh: Not on any branch and no commit yet?"
+	exit 1
+}
+
+mkdir -p ".git/rr-cache" || true
+git rev-list --parents "HEAD" |
+while read commit parent1 other_parents
+do
+	if test -z "${other_parents}"
+	then
+		# Skip non-merges
+		continue
+	fi
+	git checkout -q "${parent1}^0"
+	if git merge ${other_parents} >/dev/null 2>&1
+	then
+		# Cleanly merges
+		continue
+	fi
+	if test -s ".git/MERGE_RR"
+	then
+		git show -s --pretty=format:"Learning from %h %s" "${commit}"
+		git rerere
+		git checkout -q ${commit} -- .
+		git rerere
+	fi
+	git reset -q --hard
+done
+
+if test -z "${ORIGINAL_BRANCH}"
+then
+	git checkout "${ORIGINAL_HEAD}"
+else
+	git checkout "${ORIGINAL_BRANCH#refs/heads/}"
+fi
+
+echo "END rerere-train.sh"
+echo
 
 git merge -Xignore-all-space --no-commit ${COMMIT_TO_MERGE} || ./.github/notify_error.sh "UPSTREAM MERGE CONFLICT" $@
 git submodule update --init --recursive
