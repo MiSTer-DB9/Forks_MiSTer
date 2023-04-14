037f134e19b325d1f53b5ce285724b3fbe9c92a7
José Manuel Barroso Galindo <theypsilon@gmail.com>
2023-04-14 03:00:36 +0200

    Update sync_forks.sh

--- .github/sync_forks.sh
+++ .github/sync_forks.sh
@@ -91,9 +91,9 @@ config = ConfigParser.ConfigParser()
 config.readfp(sys.stdin)
 
 for sec in config.sections():
-    print \"declare -A %s\" % (sec)
+    print(\"declare -A %s\" % (sec))
     for key, val in config.items(sec):
-        print '%s[%s]=\"%s\"' % (sec, key, val)
+        print('%s[%s]=\"%s\"' % (sec, key, val))
 ")
 
 for fork in ${Forks[syncing_forks]}
