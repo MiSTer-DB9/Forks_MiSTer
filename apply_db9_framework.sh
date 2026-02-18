fb44109210e12a3f47063a79c8cf63d338ddda15
Timothy Redaelli <timothy.redaelli@gmail.com>
2026-02-18 16:29:54 +0100

    Fix SNAC8 framework script

--- apply_db9_framework.sh
+++ apply_db9_framework.sh
@@ -6,7 +6,7 @@ apply_db9_framework() {
     local dir="${1}"
 
     cp -r fork_ci_template/sys "${dir}/"
-    patch -Np1 -d "${dir}/" < fork_ci_template/sys.patch || :
+    git -C "${dir}/" apply --ignore-whitespace < fork_ci_template/sys.patch || :
 
     pushd "${dir}" >/dev/null
 
@@ -18,7 +18,7 @@ apply_db9_framework() {
     # Remove the old joydb files
     if git rm -f joydb9md.v joydb15.v || git rm -f rtl/joydb9md.v rtl/joydb15.v; then
         sed -i -e '/joydb9md\.v/d' -e '/joydb15\.v/d' files.qip
-        sed -i "s/^\(\s*\).joy_raw(OSD_STATUS?\s*(\(\S*\)\[5:0\]|\(\S*\)\[5:0\])\s*:\s*6'b000000\s*),.*/\1.joy_raw(OSD_STATUS? (\2[11:0] | \3[11:0]) : 11'b0),/" *.sv
+        sed -i "s/^\(\s*\).joy_raw(OSD_STATUS?\s*(\(\S*\)\[5:0\]|\(\S*\)\[5:0\])\s*:\s*6'b000000\s*),.*/\1.joy_raw(OSD_STATUS? (\2[11:0] | \3[11:0]) : 12'b0),/" *.sv
         git add files.qip
     fi
 }
